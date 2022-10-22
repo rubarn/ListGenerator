c7ea49eed061953078ba895893d3b45cb7b642cb
Ruben Kroken Rokkones <rubenkro@stud.ntnu.no>
2022-10-23 00:20:54 +0200

    bllakjf

--- src/main/java/listgenerator/List.java
+++ src/main/java/listgenerator/List.java
@@ -3,63 +3,74 @@ package listgenerator;
 import java.io.File;
 import java.io.FileInputStream;
 import java.util.Collection;
+import java.util.Iterator;
 
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.usermodel.CellType;
+
+
 
 public class List {
 
     private Collection<String> ip;
     private Collection<Collection<String>> ipList;
 
-    public List(File file, String... ips){
-        for (String string : ips) {
-            ip.add(string);
-        }
+    public List(String filePath){
+        
         try {
-            POIFSFileSystem fs = new POIFSFileSystem(new FileInputStream(file));
-            HSSFWorkbook wb = new HSSFWorkbook(fs);
-            HSSFSheet sheet = wb.getSheetAt(0);
-            HSSFRow row;
-            HSSFCell cell;
+            FileInputStream file = new FileInputStream(new File(filePath));
 
-            int rows; // No of rows
-            rows = sheet.getPhysicalNumberOfRows();
+            // Create Workbook instance holding reference to .xlsx file
+            XSSFWorkbook workbook = new XSSFWorkbook(file);
 
-            int cols = 0; // No of columns
-            int tmp = 0;
+            // Get first/desired sheet from the workbook
+            XSSFSheet sheet = workbook.getSheetAt(0);
 
-            // This trick ensures that we get the data properly even if it doesn't start
-            // from first few rows
-            for (int i = 0; i < 10 || i < rows; i++) {
-                row = sheet.getRow(i);
-                if (row != null) {
-                    tmp = sheet.getRow(i).getPhysicalNumberOfCells();
-                    if (tmp > cols)
-                        cols = tmp;
-                }
-            }
+            // Iterate through each rows one by one
+            Iterator<Row> rowIterator = sheet.iterator();
+            while (rowIterator.hasNext()) {
+                Row row = rowIterator.next();
+                // For each row, iterate through all the columns
+                Iterator<Cell> cellIterator = row.cellIterator();
 
-            for (int r = 0; r < rows; r++) {
-                row = sheet.getRow(r);
-                if (row != null) {
-                    for (int c = 0; c < cols; c++) {
-                        cell = row.getCell((short) c);
-                        if (cell != null) {
-                            // Your code here
-                        }
+                while (cellIterator.hasNext()) {
+                    Cell cell = cellIterator.next();
+                    // Check the cell type and format accordingly
+                    switch (cell.getCellType()) {
+                        case NUMERIC:
+                            System.out.print(cell.getNumericCellValue() + "t");
+                            break;
+                        case STRING:
+                            System.out.print(cell.getStringCellValue() + "t");
+                            break;
+                        case BLANK:
+                            break;
+                        case BOOLEAN:
+                            break;
+                        case ERROR:
+                            break;
+                        case FORMULA:
+                            break;
+                        case _NONE:
+                            break;
+                        default:
+                            break;
                     }
                 }
+                System.out.println("");
             }
-
+            file.close();
         } catch (Exception e) {
-            // TODO: handle exception
+            e.printStackTrace();
         }
     }
 
+    public static void main(String[] args) {
+        new List("C:/Users/ruben/Desktop/Jobb/excelips.xlsx");
+    }
 
     
 }
